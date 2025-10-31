ff700c41dbd470ac3ac49a1e943bb09a9eb625b7
Mateo Durán <mduran@prioritypower.com>
2025-10-31 15:14:38 -0500

    feat: first file creation

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask ipmort Flas, jsonity, request
+
+app = Flask(__name__
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    if(request.method == 'GET'):
+        data = {"data": "Hello, World!"}
+        return jsonify(message=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
