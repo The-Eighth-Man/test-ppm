13833aef5b179d4402d3b91e93082d3f558df3e2
Mateo Durán <mduran@prioritypower.com>
2025-10-31 16:27:59 -0500

    fix: typo

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def home():
+    data = {"data": "Hello, World!"}
+    return jsonify(message=data)
+
 @app.route('/hello', methods=['GET'])
 def hello():
-    if(request.method == 'GET'):
-        data = {"data": "Hello, World!"}
+    if request.method == 'GET':
+        data = {"data": "Hello from /hello!"}
         return jsonify(message=data)
 
 if __name__ == '__main__':
